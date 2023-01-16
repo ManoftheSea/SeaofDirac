f12a1578763e627265461bf995883899ee577e19
Derek LaHousse <derek@seaofdirac.org>
2023-01-16 14:08:46 -0500

    organization of ebin-v7

--- hosts/ebin-v7/default.nix
+++ hosts/ebin-v7/default.nix
@@ -1,79 +1,20 @@
 { config, lib, suites, profiles, pkgs, ... }:
 
 {
-  imports = suites.server;
-
-  boot.loader.efi.canTouchEfiVariables = false;
-
-  fileSystems = {
-    "/" = {
-      device = "none";
-      fsType = "tmpfs";
-      options = [ "defaults" "mode=755" ];
-    };
-    "/nix" = {
-      device = "/dev/disk/by-label/nix_store";
-      fsType = "ext4";
-    };
-    "/var" = {
-      device = "/dev/disk/by-label/var";
-      fsType = "ext4";
-    };
-    "/efi" = {
-      device = "/dev/disk/by-label/ESP";
-      fsType = "vfat";
-    };
-  };
-
-  swapDevices = [
-    {
-      device = "/dev/disk/by-label/swap";
-    }
+  imports = suites.server ++ [
+    ./filesystem.nix
+    ./network.nix
   ];
 
-  networking = {
-    hostName = "ebin-v7";
-    useNetworkd = true;
-    wireless.enable = false;
-    useDHCP = false;
-    firewall = {
-      enable = true;
-      allowPing = true;
-      trustedInterfaces = [ "br0" ];
-      allowedTCPPorts = [
-        22
-      ];
-      allowedUDPPorts = [ ];
-    };
-  };
-
-  systemd.network = {
-    enable = true;
-    netdevs.br0.netdevConfig = {
-      Name = "br0";
-      Kind = "bridge";
-    };
-    networks = {
-      eth0 = {
-        matchConfig.Name = "eth0";
-      };
-      wan = {
-        matchConfig.Name = "wan";
-        networkConfig = {
-          DHCP = "yes";
-        };
-      };
-      lan = {
-        matchConfig.Name = "lan*";
-        networkConfig = {
-          Bridge = "br0";
-        };
-      };
-    };
-  };
+  # boot.kernelModules = [ "coretemp" ];
 
   environment = {
+    # etc."sysconfig/lm_sensors".text = ''
+    #   HWMON_MODULES="coretemp"
+    # '';
+
     noXlibs = true;
+
     systemPackages = with pkgs; [
       vim
       wget
@@ -81,6 +22,9 @@
   };
 
   hardware.enableRedistributableFirmware = true;
+
+  # powerManagement.cpuFreqGovernor = "ondemand";
+  # powerManagement.powertop.enable = true;
 
   services.openssh = {
     hostKeys = [

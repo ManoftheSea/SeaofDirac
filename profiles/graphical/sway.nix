84b6fe16872af2fd173d189e23985cbfddc4278d
Derek LaHousse <derek@seaofdirac.org>
2023-01-18 11:32:03 -0500

    sway

--- profiles/graphical/sway.nix
+++ profiles/graphical/sway.nix
@@ -10,17 +10,11 @@
     wdisplays
     wev
     wf-recorder
-    wf-clipboard
+    wl-clipboard
     wofi
   ];
 
   programs.sway.enable = true;
-  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  wayland.windowManager.sway = {
-    enable = true;
-    config = {
-      # Lots of stuff to customize here
-    };
-  };
 }

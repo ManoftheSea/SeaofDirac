a4385a713b2b62defcacf4134553064ea1ea0065
Derek LaHousse <derek@seaofdirac.org>
2023-02-09 22:44:39 -0500

    swaylock in pam

--- profiles/graphical/sway.nix
+++ profiles/graphical/sway.nix
@@ -3,22 +3,26 @@
   pkgs,
   ...
 }: {
-  environment.defaultPackages = builtins.attrValues {
-    inherit
-      (pkgs)
-      grim
-      slurp
-      swappy
-      swayidle
-      swaylock
-      wdisplays
-      wev
-      wf-recorder
-      wl-clipboard
-      wofi
-      ;
+  environment = {
+    defaultPackages = builtins.attrValues {
+      inherit
+        (pkgs)
+        grim
+        slurp
+        swappy
+        swayidle
+        swaylock
+        wdisplays
+        wev
+        wf-recorder
+        wl-clipboard
+        wofi
+        ;
+    };
+    sessionVariables.NIXOS_OZONE_WL = "1";
   };
 
   programs.sway.enable = true;
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  security.pam.services.swaylock = {};
 }

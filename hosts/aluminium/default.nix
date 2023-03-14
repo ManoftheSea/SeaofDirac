adbe5dabf69effb721bf377374455716a3cd316e
Derek LaHousse <derek@seaofdirac.org>
2023-03-14 01:05:11 -0400

    add usb redirection to virt-manager

--- hosts/aluminium/default.nix
+++ hosts/aluminium/default.nix
@@ -105,6 +105,7 @@
   security.polkit.enable = true;
 
   virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
   virtualisation.docker.enable = true;
 
   zramSwap.enable = true;

329c89969d792b427c04ecb03ae3c0ea0314681b
Derek LaHousse <derek@seaofdirac.org>
2023-11-28 22:15:39 -0500

    add ipv6 to technetium

--- hosts/technetium/network.nix
+++ hosts/technetium/network.nix
@@ -23,6 +23,12 @@
         matchConfig.Name = "enp4s0f0";
         address = ["192.168.200.10/24"];
         gateway = ["192.168.200.1"];
+        ipv6AcceptRAConfig.Token = "::10";
+        networkConfig = {
+          DHCP = "no";
+          IPv6AcceptRA = true;
+          LinkLocalAddressing = "ipv6";
+        };
       };
     };
   };

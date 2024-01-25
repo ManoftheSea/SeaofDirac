93345c267f8c8b57ecb29dacf2bdba8195fe8359
Derek LaHousse <derek@seaofdirac.org>
2024-01-25 14:02:24 -0500

    turn off channels

--- profiles/core/flakes.nix
+++ profiles/core/flakes.nix
@@ -1,5 +1,6 @@
 {nixos, ...}: {
   nix = {
+    channel.enable = false;
     nixPath = ["nixpkgs=${nixos.outPath}"];
     registry.nixpkgs.flake = nixos;
     settings.experimental-features = ["nix-command" "flakes"];

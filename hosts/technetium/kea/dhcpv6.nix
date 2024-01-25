5333272a57c76d00a059864527939a289004de3b
Derek LaHousse <derek@seaofdirac.org>
2024-01-25 09:39:34 -0500

    add timezones

--- hosts/technetium/kea/dhcpv6.nix
+++ hosts/technetium/kea/dhcpv6.nix
@@ -21,16 +21,29 @@
       interfaces-config.interfaces = ["enp4s0f0/2601:5cd:c100:3940::10"];
       option-data = [
         {
+          # code = 12;
+          name = "unicast";
+          data = "2601:5cd:c100:3940::10";
+        }
+        {
+          # code = 23;
           name = "dns-servers";
           data = "2601:5cd:c100:3940::5";
         }
         {
+          # code = 24;
           name = "domain-search";
           data = "seaofdirac.org";
         }
         {
-          name = "unicast";
-          data = "2601:5cd:c100:3940::10";
+          # code = 41;
+          name = "new-posix-timezone";
+          data = "EST5EDT4,M3.2.0/01:00:00,M11.1.0/02:00:00";
+        }
+        {
+          # code = 42;
+          name = "new-tzdb-timezone";
+          data = "America/New_York";
         }
       ];
       subnet6 = [

bc12e00d24e773f8757883dadd0735a9cf6767bb
Derek LaHousse <derek@seaofdirac.org>
2024-05-08 17:50:23 -0400

    dhcpv6 configuration

--- hosts/technetium/kea/dhcpv6.nix
+++ hosts/technetium/kea/dhcpv6.nix
@@ -2,49 +2,47 @@
   services.kea.dhcp6 = {
     enable = true;
     settings = {
-      /*
-      control-socket = { };
+      # control-socket = {};
       hosts-database = {
         type = "postgresql";
         name = "kea";
         host = "/run/postgresql";
         user = "kea";
       };
+      lease-database.type = "memfile";
+      /*
       lease-database = {
         type = "postgresql";
         name = "kea";
         host = "/run/postgresql";
         user = "kea";
       };
-      lease-database {
-        type = "memfile";
-      };
       */
       /*
       client-classes = [
         {
           name = "ipxe_efi_amd64";
-          test = "option[93].hex == 0x0009"; #x86_64
+          test = "option[61].hex == 0x0009"; #x86_64
           next-server = "192.168.200.10";
           boot-file-name = "/bootloaders/netboot.xyz.kpxe";
           only-if-required = true;
         }
         {
           name = "ipxe_efi_arm64";
-          test = "option[93].hex == 0x000b"; #ARM64
+          test = "option[61].hex == 0x000b"; #ARM64
           next-server = "192.168.200.10";
           boot-file-name = "/bootloaders/netboot.xyz-arm64.efi";
           only-if-required = true;
         }
         {
           name = "http_efi_amd64";
-          test = "option[93].hex == 0x0010"; #x86_64
+          test = "option[61].hex == 0x0010"; #x86_64
           boot-file-name = "http://technetium.seaofdirac.org/bootloaders/netboot.xyz.efi";
           only-if-required = true;
         }
         {
           name = "http_efi_arm64";
-          test = "option[93].hex == 0x0013"; #ARM64
+          test = "option[61].hex == 0x0013"; #ARM64
           boot-file-name = "http://technetium.seaofdirac.org/bootloaders/netboot.xyz-arm64.efi";
           only-if-required = true;
         }
@@ -82,21 +80,21 @@
       ];
       subnet6 = [
         {
-          id = 80;
+          id = 200;
           subnet = "2601:5cd:c100:3940::/64";
         }
         {
-          id = 81;
-          subnet = "2601:5cd:c100:3941::/64";
-        }
-        {
+          id = 101;
           ddns-qualifying-suffix = "users.seaofdirac.org";
-          id = 82;
+          # interface-id from ICX6250 appears to be:
+          # 2 bytes VLAN id, 1 byte ethernet port? 1 byte unknown
+          # 00 65 21 02 (or 101, 33, 2) for dhcp on VLAN 101
+          # interface-id = "ve 101";
           subnet = "2601:5cd:c100:3942::/64";
         }
         {
-          id = 83;
-          subnet = "2601:5cd:c100:3943::/64";
+          id = 205;
+          subnet = "fd50:63ed:f2b7:1cd::/64";
         }
       ];
     };

ba38a2edc6b9a81c1f9bdaf262df6c1f6da1e779
Derek LaHousse <derek@seaofdirac.org>
2024-06-03 12:39:24 -0400

    complete nextcloud upgrade to 29

--- hosts/technetium/nextcloud.nix
+++ hosts/technetium/nextcloud.nix
@@ -25,7 +25,7 @@
       #};
       https = true;
       hostName = "nextcloud.seaofdirac.org";
-      package = pkgs.nextcloud28;
+      package = pkgs.nextcloud29;
       # phpOptions = { upload_max_filesize = "1G"; post_max_size = "1G"; };
     };
 

4f6aab2b3996139105201c26cd6ebc1a2bed2340
Derek LaHousse <derek@seaofdirac.org>
2024-06-03 10:45:46 -0400

    switch nickel to plasma6

--- hosts/nickel/services.nix
+++ hosts/nickel/services.nix
@@ -17,7 +17,7 @@
 
     xserver = {
       enable = true;
-      desktopManager.plasma5.enable = true;
+      desktopManager.plasma6.enable = true;
       displayManager.sddm.enable = true;
       libinput.enable = true;
     };

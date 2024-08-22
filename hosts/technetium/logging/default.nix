aeafb2640b2d06cce61a96618d504c0fc302605e
Derek LaHousse <derek@seaofdirac.org>
2024-08-22 09:29:57 -0400

    technetium: add vector for udp syslog

--- hosts/technetium/logging/default.nix
+++ hosts/technetium/logging/default.nix
@@ -1,5 +1,6 @@
 _: {
   imports = [
     ./loki.nix
+    ./vector.nix
   ];
 }
---
+++ hosts/technetium/logging/vector.nix
@@ -0,0 +1,25 @@
+_: {
+  networking.firewall.allowedUDPPorts = [514];
+
+  services.vector = {
+    enable = true;
+    settings = {
+      sources.syslog-udp-listener = {
+        type = "syslog";
+        address = "192.168.200.10:514";
+        mode = "udp";
+      };
+      sinks.loki = {
+        type = "loki";
+        inputs = ["syslog-udp-listener"];
+        endpoint = "http://localhost:3100";
+        encoding.codec = "json";
+        labels = {
+          source = "syslog";
+          protocol = "udp";
+          transport = "vector";
+        };
+      };
+    };
+  };
+}

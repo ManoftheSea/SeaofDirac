83fbfb2965f251d1b5b2af44c8ae926e1c1aa96b
Derek LaHousse <derek@seaofdirac.org>
2025-03-17 12:44:00 -0400

    factorio: version bump

--- overlays/factorio.nix
+++ overlays/factorio.nix
@@ -1,26 +1,26 @@
 _: {
   modifications = _final: prev: let
-    versionsJson = prev.writeText "factorio-2.0.10.json" ''
+    versionsJson = prev.writeText "factorio-2.0.32.json" ''
       {
         "x86_64-linux": {
           "alpha": {
             "stable": {
-              "name": "factorio_linux_2.0.10.tar.xz",
+              "name": "factorio_linux_2.0.32.tar.xz",
               "needsAuth": true,
-              "sha256": "16rrbia0iv9lp5rbfwnqkc1nwcpacdladppyb9fyz59g272qyl07",
+              "sha256": "07v1rw2bh5sgscmby12j29y3pw9nmpn3cbbl5gh10zdydfc38d2c",
               "tarDirectory": "x64",
-              "url": "https://factorio.com/get-download/2.0.10/alpha/linux64",
-              "version": "2.0.10"
+              "url": "https://factorio.com/get-download/2.0.32/alpha/linux64",
+              "version": "2.0.32"
             }
           },
           "headless": {
             "stable": {
-              "name": "factorio-headless_linux_2.0.10.tar.xz",
+              "name": "factorio-headless_linux_2.0.32.tar.xz",
               "needsAuth": false,
-              "sha256": "1x13ihgywn9i6hkyfz5zly7rky1sb5yssfz3llc54wbgz89d4z9d",
+              "sha256": "05v4hlnlsp8y60hy3k9aj4srwmka6b6qpmivjbzfiifw8ap04q9a",
               "tarDirectory": "x64",
-              "url": "https://factorio.com/get-download/2.0.10/headless/linux64",
-              "version": "2.0.10"
+              "url": "https://factorio.com/get-download/2.0.32/headless/linux64",
+              "version": "2.0.32"
             }
           }
         }

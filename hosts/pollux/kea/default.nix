9c66e406d35f50fb2f8615ea170824807bb1edcd
Derek LaHousse <derek@seaofdirac.org>
2025-04-04 17:08:01 -0400

    castor, pollux, kea: permissions of config files, LoadCredentials

--- hosts/pollux/kea/default.nix
+++ hosts/pollux/kea/default.nix
@@ -12,16 +12,15 @@
   services.kea = {
     dhcp4 = {
       enable = true;
-      #configFile = "/run/credentials/kea-dhcp4-server/dhcpv4.conf";
-      configFile = config.sops.secrets.kea-dhcpv4.path;
+      configFile = "/run/credentials/kea-dhcp4-server.service/dhcpv4.conf";
     };
     dhcp6 = {
       enable = false;
-      configFile = config.sops.secrets.kea-dhcpv6.path;
+      configFile = "/run/credentials/kea-dhcp6-server.service/dhcpv6.conf";
     };
     dhcp-ddns = {
       enable = true;
-      configFile = config.sops.secrets.kea-ddns.path;
+      configFile = "/run/credentials/kea-dhcp-ddns-server.service/dhcp-ddns.conf";
     };
   };
 
@@ -30,25 +29,25 @@
       format = "json";
       sopsFile = ./dhcpv4.json;
       key = "";
-      mode = "0444";
+      mode = "0440";
     };
     kea-dhcpv6 = {
       format = "json";
       sopsFile = ./dhcpv6.json;
       key = "";
-      mode = "0444";
+      mode = "0440";
     };
     kea-ddns = {
       format = "json";
       sopsFile = ./dhcp-ddns.json;
       key = "";
-      mode = "0444";
+      mode = "0440";
     };
   };
 
   systemd.services = {
     kea-dhcp4-server.serviceConfig.LoadCredential = "dhcpv4.conf:${config.sops.secrets.kea-dhcpv4.path}";
     kea-dhcp6-server.serviceConfig.LoadCredential = "dhcpv6.conf:${config.sops.secrets.kea-dhcpv6.path}";
-    kea-dhcp-ddns-server.serviceConfig.LoadCredential = "dhcpv4.conf:${config.sops.secrets.kea-dhcpv4.path}";
+    kea-dhcp-ddns-server.serviceConfig.LoadCredential = "dhcp-ddns.conf:${config.sops.secrets.kea-ddns.path}";
   };
 }

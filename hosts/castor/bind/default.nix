bd414ea241b4aa77d8bf873a9ee1a6fd8bd289b8
Derek LaHousse <derek@seaofdirac.org>
2025-04-04 17:54:15 -0400

    bind: add bind9 to castor to replace iron

---
+++ hosts/castor/bind/default.nix
@@ -0,0 +1,140 @@
+{
+  self,
+  config,
+  lib,
+  ...
+}: {
+  networking.firewall = lib.mkIf config.services.bind.enable {
+    allowedTCPPorts = [
+      53 # named
+      953 # rndc
+    ];
+    allowedUDPPorts = [
+      53 # named
+    ];
+  };
+
+  services.bind = {
+    enable = true;
+    cacheNetworks = [
+      "internal"
+    ];
+    extraOptions = ''
+      response-policy { zone "rpz.blocklist"; };
+
+      dns64 64:ff9b::/96 {
+        clients { !translator; dns64-good-clients; };
+        mapped { !rfc1918; !rfc6598; any; };
+        exclude { !ula-prefixes; 0::/3; 4000::/2; 8000::/1; 2001:db8::/32; };
+        break-dnssec yes;
+        recursive-only yes;
+      };
+    '';
+    extraConfig = ''
+      include "${config.sops.secrets."bind/config/acls".path}";
+      include "${config.sops.secrets."bind/config/controls".path}";
+      include "${config.sops.secrets."bind/rndc_keys/aluminium".path}";
+      include "${config.sops.secrets."bind/rndc_keys/ddns-key".path}";
+    '';
+
+    zones = {
+      "${config.networking.domain}" = {
+        file = "/var/dns/${config.networking.domain}.db";
+        master = true;
+        slaves = ["trusted"];
+        extraConfig = ''
+          update-policy {
+            grant aluminium zonesub any;
+          };
+        '';
+      };
+      "users.${config.networking.domain}" = {
+        file = "/var/dns/users.${config.networking.domain}.db";
+        master = true;
+        slaves = ["trusted"];
+        extraConfig = ''
+          update-policy {
+            grant ddns-key.seaofdirac.org. zonesub any;
+            grant ddns.technetium.seaofdirac.org. subdomain users.seaofdirac.org. any;
+          };
+        '';
+      };
+      "0.1.c.d.c.5.0.1.0.6.2.ip6.arpa" = {
+        file = "/var/dns/2601.5cd.c10-pd-reverse.db";
+        master = true;
+        slaves = ["trusted"];
+        extraConfig = ''
+          update-policy {
+            grant ddns-key.seaofdirac.org. zonesub any;
+          };
+        '';
+      };
+      "168.192.in-addr.arpa" = {
+        file = "/var/dns/192.168.db";
+        master = true;
+        slaves = ["trusted"];
+        extraConfig = ''
+          update-policy {
+            grant ddns-key.seaofdirac.org. zonesub any;
+          };
+        '';
+      };
+      "20.172.in-addr.arpa" = {
+        file = "/var/dns/172.20.db";
+        master = true;
+        slaves = ["trusted"];
+        extraConfig = ''
+          update-policy {
+            grant ddns-key.seaofdirac.org. zonesub any;
+          };
+        '';
+      };
+      "10.in-addr.arpa" = {
+        file = "/var/dns/10.db";
+        master = true;
+        slaves = ["trusted"];
+        extraConfig = ''
+          update-policy {
+            grant ddns-key.seaofdirac.org. zonesub any;
+          };
+        '';
+      };
+      "rpz.blocklist" = {
+        file = "/var/dns/rpz.blocklist";
+        master = true;
+        slaves = ["trusted"];
+        extraConfig = ''
+          update-policy {};
+        '';
+      };
+    };
+  };
+
+  sops.secrets =
+    lib.mkIf config.services.bind.enable
+    (lib.genAttrs [
+        "bind/rndc_keys/aluminium"
+        "bind/rndc_keys/ddns-key"
+        "bind/rndc_keys/technetium"
+        "bind/config/acls"
+        "bind/config/controls"
+      ] (_: {
+        owner = config.users.users.named.name;
+        sopsFile = "${self}/hosts/secrets/bind.yaml";
+      }));
+
+  # These are required in 24.11, but part of the definition in unstable (20250405)
+  systemd.services.bind.serviceConfig = {
+    AmbientCapabilities = "CAP_NET_BIND_SERVICE";
+    CapabilityBoundingSet = "CAP_NET_BIND_SERVICE";
+    User = lib.mkIf config.services.bind.enable "named";
+  };
+
+  systemd.tmpfiles.settings = lib.mkIf config.services.bind.enable {
+    bind-zones."/var/dns".d = {
+      group = "named";
+      mode = "750";
+      user = "named";
+    };
+  };
+}

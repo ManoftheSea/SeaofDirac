bf58a8066fd40a402ba8ebad1b57ab7a8c696465
Derek LaHousse <derek@seaofdirac.org>
2025-08-04 15:07:45 -0400

    littlecreek: well-known in nix rather than directory/files

--- hosts/littlecreek/services/web-server/mta-sts.nix
+++ hosts/littlecreek/services/web-server/mta-sts.nix
@@ -4,17 +4,21 @@
   ...
 }: let
   inherit (config.networking) domain fqdn;
+  mta-sts = ''
+    version: STSv1
+    mode: enforce
+    max_age: 10368000
+    mx: littlecreek.seaofdirac.org
+  '';
 in {
   security.acme.certs."${fqdn}".extraDomainNames = ["mta-sts.${domain}"];
 
   services.nginx.virtualHosts."mta-sts.${domain}" = {
-    root = pkgs.writeTextDir ".well-known/mta-sts.txt" ''
-      version: STSv1
-      mode: enforce
-      max_age: 10368000
-      mx: littlecreek.seaofdirac.org
-    '';
     forceSSL = true;
+    locations."= /.well-known/mta-sts.txt".extraConfig = ''
+      default_type text/plain;
+      return 200 ${pkgs.lib.strings.toJSON mta-sts};
+    '';
     useACMEHost = fqdn;
   };
 }

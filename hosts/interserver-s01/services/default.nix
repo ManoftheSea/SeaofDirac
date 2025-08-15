9bab1eac74f9964d2c6e03b27a6049645b4e8025
Derek LaHousse <derek@seaofdirac.org>
2025-08-15 13:35:46 -0400

    interserver-s01: set up garage to provide S3 object storage

--- hosts/interserver-s01/services/default.nix
+++ hosts/interserver-s01/services/default.nix
@@ -1,2 +1,5 @@
 _: {
+  imports = [
+    ./garage.nix
+  ];
 }
---
+++ hosts/interserver-s01/services/garage.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  networking.firewall.allowedTCPPorts = [3900];
+
+  services.garage = {
+    enable = true;
+    environmentFile = config.sops.secrets.garage-env.path;
+    package = pkgs.garage_1_x;
+    settings = {
+      admin.api_bind_addr = "[::1]:3903";
+      replication_factor = 1;
+      rpc_bind_addr = "[::]:3901";
+      rpc_public_addr_subnet = "2601:5cd:c100:3940::/64";
+      s3_api = {
+        api_bind_addr = "[::]:3900";
+        s3_region = "us-east-1";
+      };
+    };
+  };
+
+  sops.secrets.garage-env = {};
+}

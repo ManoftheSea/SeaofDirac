9a8f4a19c6e7e8ddfdad11f74c3daa8d96b3c942
Derek LaHousse <derek@seaofdirac.org>
2025-12-01 10:46:24 -0500

    core: update to 25.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     ### Official NixOS Package Sources ###
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Official hardware configurations
@@ -26,10 +26,9 @@
 
     # Declarative mail server with postfix and dovecot
     snm = {
-      url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-25.05";
+      url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-25.11";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nixpkgs-25_05.follows = "nixpkgs";
         flake-compat.follows = "deploy-rs/flake-compat";
       };
     };

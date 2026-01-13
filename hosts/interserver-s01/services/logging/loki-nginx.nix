ff883fbec67a6a504e178451d634802c89718c50
Derek LaHousse <derek@seaofdirac.org>
2026-01-13 17:35:49 -0500

    interserver-s01: use DNS challenge, not HTTP, for acme certs

--- hosts/interserver-s01/services/logging/loki-nginx.nix
+++ hosts/interserver-s01/services/logging/loki-nginx.nix
@@ -4,11 +4,10 @@ in {
   services.nginx = {
     upstreams.loki.servers."localhost:${toString config.services.loki.configuration.server.http_listen_port}" = {};
     virtualHosts.${fqdn} = {
-      enableACME = true;
       locations."/" = {
         extraConfig = ''
           allow 73.31.234.197;
-          allow 2601:5cc:4a01:6fa0::/60;
+          allow 2601:5cc:4a00:61d0::/60;
           deny  all;
         '';
         proxyPass = "http://loki";

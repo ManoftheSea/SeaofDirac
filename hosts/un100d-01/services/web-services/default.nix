f8890c9e252205bcf055612c38ad97a35e85e774
Derek LaHousse <derek@seaofdirac.org>
2026-01-12 23:25:12 -0500

    un100d-01: initial add home-assistant

--- hosts/un100d-01/services/web-services/default.nix
+++ hosts/un100d-01/services/web-services/default.nix
@@ -1,6 +1,7 @@
 _: {
   imports = [
+    ./home-asst.nix
     ./nginx.nix
-    ./netbox.nix
+    # ./netbox.nix
   ];
 }
---
+++ hosts/un100d-01/services/web-services/home-asst.nix
@@ -0,0 +1,55 @@
+{config, ...}: let
+  # inherit (config.networking) domain fqdn;
+  domain = "seaofdirac.org";
+  fqdn = "${config.networking.hostName}.${domain}";
+in {
+  security.acme.certs."${fqdn}".extraDomainNames = ["home.${domain}"];
+
+  services = {
+    home-assistant = {
+      enable = true;
+      config = {
+        default_config = {};
+        http = {
+          server_host = "::1";
+          trusted_proxies = ["::1"];
+          use_x_forwarded_for = true;
+        };
+        recorder.db_url = "postgresql://@/hass";
+      };
+      extraComponents = [
+        # Components required to complete the onboarding
+        "analytics"
+        "google_translate"
+        "met"
+        "radio_browser"
+        "shopping_list"
+        "isal"
+      ];
+      extraPackages = p: [p.psycopg2];
+    };
+
+    nginx.virtualHosts."home.${domain}" = {
+      extraConfig = ''
+        proxy_buffering off;
+      '';
+      locations."/" = {
+        proxyPass = "http://[${config.services.home-assistant.config.http.server_host}]:${builtins.toString config.services.home-assistant.config.http.server_port}";
+        proxyWebsockets = true;
+      };
+      forceSSL = true;
+      useACMEHost = fqdn;
+    };
+
+    postgresql = {
+      enable = true;
+      ensureDatabases = ["hass"];
+      ensureUsers = [
+        {
+          name = "hass";
+          ensureDBOwnership = true;
+        }
+      ];
+    };
+  };
+}

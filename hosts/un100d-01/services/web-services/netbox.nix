39961e909266cc738ae92f42c8cd7cba767e565d
Derek LaHousse <derek@seaofdirac.org>
2026-05-27 17:13:06 -0400

    un100d-01: add custom filter to netbox

--- hosts/un100d-01/services/web-services/netbox.nix
+++ hosts/un100d-01/services/web-services/netbox.nix
@@ -14,10 +14,42 @@ in {
   services = {
     netbox = {
       enable = true;
-      extraConfig = ''
-        with open("/run/secrets/netbox/password", "r") as file:
-          DATABASE["PASSWORD"] = file.readline()
-      '';
+      extraConfig =
+        ''
+          with open("/run/secrets/netbox/password", "r") as file:
+            DATABASE["PASSWORD"] = file.readline()
+
+        ''
+        + ''
+          def list_to_ranges(lst):
+            ranges = []
+            start = lst[0]
+            end = lst[0]
+
+            for interface in lst[1:]:
+                i = interface.rsplit('/', 1)
+                e = end.rsplit('/', 1)
+
+                if i[0] == e[0] and int(i[1]) == int(e[1]) + 1:
+                    end = interface
+                else:
+                    if start == end:
+                        ranges.append(str(start))
+                    else:
+                        ranges.append(f"{start} to {end}")
+                    start = interface
+                    end = interface
+
+            if start == end:
+                ranges.append(str(start))
+            else:
+                ranges.append(f"{start} to {end}")
+
+            return ranges
+
+          JINJA2_FILTERS = {"list_to_ranges": list_to_ranges}
+
+        '';
       package = self.packages.${pkgs.system}.netbox_4_4;
       plugins = p: [
         p.netbox-dns
